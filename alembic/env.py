9efd55df606f71a3fad0c2cfd17b26a7f4f6d84d
mita owner <118620853+teniee@users.noreply.github.com>
2025-06-11 22:08:35 +0300

    Clean analytics schemas and services

--- alembic/env.py
+++ alembic/env.py
@@ -1,18 +1,23 @@
 
-from logging.config import fileConfig
-from sqlalchemy import engine_from_config, pool
-from alembic import context
-import sys
 import os
+import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from app.db.models import Base  # Импорт моделей
+sys.path.insert(
+    0,
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "..")),
+)  # noqa: E402
+
+from logging.config import fileConfig  # noqa: E402
+from sqlalchemy import engine_from_config, pool  # noqa: E402
+from alembic import context  # noqa: E402
+from app.db.models import Base  # Импорт моделей  # noqa: E402
 
 # this is the Alembic Config object
 config = context.config
 fileConfig(config.config_file_name)
 
 target_metadata = Base.metadata
+
 
 def run_migrations_offline():
     context.configure(
@@ -23,6 +28,7 @@ def run_migrations_offline():
     )
     with context.begin_transaction():
         context.run_migrations()
+
 
 def run_migrations_online():
     connectable = engine_from_config(
@@ -38,6 +44,7 @@ def run_migrations_online():
         )
         with context.begin_transaction():
             context.run_migrations()
+
 
 if context.is_offline_mode():
     run_migrations_offline()
